1fa60d80ba973c8b6d9c0ea935d41fa9da9598c8
Marc985 <hei.marc.3@gmail.com>
2025-05-15 20:55:14 +0300

    chore:separate menu list from the main code

--- main.ts
+++ main.ts
@@ -1,25 +1,5 @@
 import {  createInterface } from "readline";
-const menuItems = [
-  '1. Transférer argent',
-  '2. SOS Crédit',
-  '3. Paiement marchand',
-  '4. Recharger crédit',
-  '5. Payer facture',
-  '6. Historique',
-  '7. Paramètres',
-  '8. Aide'
-];
-
-const subMenus: Record<number, string[]> = {
-  1: ['1. Vers MVola', '2. Vers banque', '3. Vers carte'],
-  2: ['1. Demander SOS', '2. Envoyer SOS'],
-  3: ['1. Scanner QR', '2. Saisir ID marchand'],
-  4: ['1. Tel perso', '2. Tel autre'],
-  5: ['1. Jirama', '2. Telma', '3. Canal+'],
-  6: ['1. Derniers transferts', '2. Tout voir'],
-  7: ['1. Modifier code', '2. Langue', '3. Notifications'],
-  8: ['1. Contacter support', '2. FAQ']
-};
+import { menuItems ,subMenus} from "./menu-list";
 
 const rl =createInterface({
   input: process.stdin,
